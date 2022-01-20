256b533d2e6ffd4e852619764130168e92cd3ef9
Loïc Deketelaere <zehsrafe@gmail.com>
2022-01-20 17:41:23 +0100

    Folder creation sucessful

--- src/be/intecbrussel/GetFileExtensionDemo.java
+++ src/be/intecbrussel/GetFileExtensionDemo.java
@@ -2,6 +2,7 @@ package be.intecbrussel;
 
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,37 +12,36 @@ public class GetFileExtensionDemo {
     public static void main(String[] args) {
         Path path = Paths.get("unsorted");
 
-        System.out.println(Files.walk(path));
-//        createDirectories(path);
-
+        createDirectories(getFileExtentions(path));
     }
 
-    private static void fileReader() {
-
-
-    }
-
-    private static void createDirectories(Path path){
-        String[] fileList = path.toFile().list();
-        String fileExtensions;
-        for (String s : fileList) {
-            if (s.contains(".") && !s.startsWith(".")) {
-//                int indexDot = s.charAt('.'); // not working but why?
-                int indexDot = s.lastIndexOf('.');
-                fileExtensions = s.substring(indexDot + 1);
-                try {
-                    Files.createDirectories(Path.of(Paths.get("unsorted/createFolders/") + fileExtensions));
-                } catch (IOException e){
-                    e.printStackTrace();
-                }
+    private static Set<String> getFileExtentions(Path path) {
+        try {
+            Set<String> fileExtensions = new HashSet<>();
+            int indexDot;
+            Path[] colectedFiles = Files.walk(path)
+                    .filter(s -> s.toString().contains("."))
+                    .filter(s -> !s.startsWith("."))
+                    .toArray(Path[]::new);
+            for (Path c : colectedFiles){
+                indexDot = c.toString().lastIndexOf('.');
+                fileExtensions.add(c.toString().substring(indexDot + 1));
             }
+            return fileExtensions;
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    private static void createDirectories(Set<String> fileExtensions){
+        try {
+            for (String s : fileExtensions){
+                Files.createDirectories(Path.of("unsorted/createFolders/" + s));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-
-//    private static ArrayList<String> getExtensionFile(Path path) {
-
-//        return fileExtensions;
-//        System.out.println(fileExtensions);
-//        System.out.println(Arrays.toString(fileList));
-//    }
 }
